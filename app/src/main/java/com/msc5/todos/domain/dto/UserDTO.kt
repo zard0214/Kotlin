11ff23a36165fe4ce699ab8cfe05c60dbce2f1ad
Zhecheng Zhao <zzhao84@sheffield.ac.uk>
2023-10-30 11:03:38 +0000

    app init

--- app/src/main/java/com/msc5/todos/domain/dto/UserDTO.kt
+++ app/src/main/java/com/msc5/todos/domain/dto/UserDTO.kt
@@ -1,19 +1,14 @@
 package com.msc5.todos.domain
 
-import com.google.gson.annotations.SerializedName
 
 data class UserDTO (
 
-    @SerializedName("id")
     var id: Int,
 
-    @SerializedName("username")
     var username: String,
 
-    @SerializedName("password")
     var password: String,
 
-    @SerializedName("email")
     var email: String,
 
 )
